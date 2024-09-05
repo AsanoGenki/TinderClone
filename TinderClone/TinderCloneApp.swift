59cd751b6e8b2da9b3ecfff7a2119b6ef287fdcd
AsanoGenki <asanogenki0218@icloud.com>
2024-09-05 22:52:33 +0900

    Initial commit.

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Genki on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
